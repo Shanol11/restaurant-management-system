2e6bd7dc19e27432cf714dbbcf0cd58da1bb8436
Shanol11 <109813804+Shanol11@users.noreply.github.com>
2024-06-29 20:20:30 +0530

    Update submit_booking.php

--- submit_booking.php
+++ submit_booking.php
@@ -3,7 +3,7 @@
 	$phone = $_POST['phone'];
 	$guests = $_POST['guests'];
 	$date = $_POST['date'];
-	$timeslot = $_POST['timeslot'];
+	$timeslot = $_POST['time_slot'];
 
 	// Database connection
 	$conn = new mysqli('demo','root','','restaurant');
@@ -11,8 +11,8 @@
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into table(name,phone,guests,date,timeslot) values(?, ?, ?, ?, ?, ?)");
-		$stmt->bind_param("siiii", $name, $phone, $guests, $date, $timeslot);
+		$stmt = $conn->prepare("insert into table(name,phone,guests,date,time_slot) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("siiii", $name, $phone, $guests, $date, $time_slot);
 		$execval = $stmt->execute();
 		echo $execval;
 		echo "Registration successfully...";
